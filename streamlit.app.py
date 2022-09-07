c02982592e10d587e509ce6a29fa394e3ee597a9
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 01:09:24 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -18,16 +18,16 @@ option = st.selectbox('select database:',dbs)
 st.write('Selected Database :', option)
 
 sc= pd.read_sql("select schema_name as schema,catalog_name as database from SNOWFLAKE.ACCOUNT_USAGE.SCHEMATA where deleted is NULL and catalog_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn) 
-scl = sc.loc[sc['DATABASE']==option] 
-scl.reset_index()
+scl = sc.loc[sc['DATABASE']==option].reset_index()
+scl
 scs = list(set(list(scl['SCHEMA'])))
 next = st.selectbox('select schema:',scs)
 st.write('Selected Schema:', next)
 
 tab = pd.read_sql("select table_name,table_schema as schema,table_catalog as database from SNOWFLAKE.ACCOUNT_USAGE.TABLES where deleted is NULL and table_catalog not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
 tabl1 = tab.loc[tab['SCHEMA']==next]
-tabl2 = tabl1.loc[tabl1['DATABASE']==option]
-tabl2.reset_index()
+tabl2 = tabl1.loc[tabl1['DATABASE']==option].reset_index()
+tabl2
 tabs = list(set(list(tabl2['TABLE_NAME'])))
 final = st.selectbox('select table:',tabs)
 st.write('Selected Table:', final)
