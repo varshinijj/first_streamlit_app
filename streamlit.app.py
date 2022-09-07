0061e22dab2519de8dea7af8e4bdf491b74298e4
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 01:11:03 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -33,7 +33,7 @@ final = st.selectbox('select table:',tabs)
 st.write('Selected Table:', final)
 
 dis = pd.read_sql("select COLUMN_NAME,TAG_NAME,TAG_VALUE,OBJECT_NAME from snowflake.account_usage.tag_references;",conn)
-disp = dis.loc[dis['OBJECT_NAME']==final][['COLUMN_NAME','TAG_NAME','TAG_VALUE']]
+disp = dis.loc[dis['OBJECT_NAME']==final][['COLUMN_NAME','TAG_NAME','TAG_VALUE']].reset_index(drop=True)
 disp
 
 
