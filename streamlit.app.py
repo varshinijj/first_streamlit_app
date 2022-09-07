002fbff184b0dbcaf7355b2c76b167f53fb4fa20
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 00:09:33 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -12,13 +12,18 @@ conn = snowflake.connector.connect(
                 schema='ACCOUNT_USAGE',
     ocsp_fail_open=False
                 )
-dbs = pd.read_sql("select database_name from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where deleted is NULL and database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
+dbs = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where deleted is NULL and database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
 
 option = st.selectbox('select database:',dbs)
 st.write('You selected:', option)
 
-sc= pd.read_sql("select schema_name,catalog_name from SNOWFLAKE.ACCOUNT_USAGE.SCHEMATA where deleted is NULL and catalog_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn) 
-sc.loc[sc['CATALOG_NAME']==option] 
+sc= pd.read_sql("select schema_name as schema,catalog_name as database from SNOWFLAKE.ACCOUNT_USAGE.SCHEMATA where deleted is NULL and catalog_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn) 
+sc.loc[sc['DATABASE']==option] 
+scs = sc.loc['SCHEMA']
+next = st.selectbox('select schema:',scs)
+st.write('You selected:', next)
+tab = pd.read_sql("select table_name as table,table_schema as schema,table_catalog as database from "SNOWFLAKE"."ACCOUNT_USAGE"."TABLES" where deleted is NULL and table_catalog not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
+tab.loc[tab['SCHEMA']==next]
 
 
 
