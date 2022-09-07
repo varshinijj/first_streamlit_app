8a46880a6a0b67c681248e5a7be3092c9ffb326d
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-07 19:07:13 +0530

    Update streamlit.app2.py

--- streamlit.app2.py
+++ streamlit.app2.py
@@ -1,1 +1,7 @@
-
+conda create -n snowflake python=3.8
+conda activate snowflake
+pip install streamlit
+pip install snowflake-sqlalchemy
+pip install streamlit-aggrid
+pip install graphviz
+pip install st-annotated-text
