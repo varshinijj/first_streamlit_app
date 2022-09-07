dc5285e0e6670df4b0f2f5a3852cd9b90a964bec
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 01:04:15 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -15,14 +15,14 @@ conn = snowflake.connector.connect(
 db = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where deleted is NULL and database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
 dbs = list(set(list(db['DATABASE'])))
 option = st.selectbox('select database:',dbs)
-st.write('You selected:', option)
+st.write('Selected Database :', option)
 
 sc= pd.read_sql("select schema_name as schema,catalog_name as database from SNOWFLAKE.ACCOUNT_USAGE.SCHEMATA where deleted is NULL and catalog_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn) 
 scl = sc.loc[sc['DATABASE']==option] 
 scl
 scs = list(set(list(scl['SCHEMA'])))
 next = st.selectbox('select schema:',scs)
-st.write('You selected:', next)
+st.write('Selected Schema:', next)
 
 tab = pd.read_sql("select table_name,table_schema as schema,table_catalog as database from SNOWFLAKE.ACCOUNT_USAGE.TABLES where deleted is NULL and table_catalog not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
 tabl1 = tab.loc[tab['SCHEMA']==next]
@@ -30,7 +30,7 @@ tabl2 = tabl1.loc[tabl1['DATABASE']==option]
 tabl2
 tabs = list(set(list(tabl2['TABLE_NAME'])))
 final = st.selectbox('select schema:',tabs)
-st.write('You selected:', final)
+st.write('Selected Table:', final)
 
 dis = pd.read_sql("select COLUMN_NAME,TAG_NAME,TAG_VALUE,OBJECT_NAME from snowflake.account_usage.tag_references;",conn)
 disp = dis.loc[dis['OBJECT_NAME']==final][['COLUMN_NAME','TAG_NAME','TAG_VALUE']]
