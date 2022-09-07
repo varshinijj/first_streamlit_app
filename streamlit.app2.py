ea6a87aa5e9ada4a3d9f96fd18f4b7afe67c5562
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-07 20:22:01 +0530

    Delete streamlit.app2.py

--- streamlit.app2.py
+++
@@ -1,7 +0,0 @@
-conda create -n snowflake python=3.8
-conda activate snowflake
-pip install streamlit
-pip install snowflake-sqlalchemy
-pip install streamlit-aggrid
-pip install graphviz
-pip install st-annotated-text
