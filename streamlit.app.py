b703f1c6fa3190738f3fac219fa1c1ecc1a29a9d
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-08 22:45:50 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -23,29 +23,29 @@ else:
   conn.cursor().execute("alter warehouse UI set warehouse_size = LARGE MAX_CLUSTER_COUNT =2 MIN_CLUSTER_COUNT =1;")
 
     
-db = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where deleted is NULL and database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
-dbs = list(set(list(db['DATABASE'])))
-option = st.selectbox('select database:',dbs)
-st.write('Selected Database :', option)
+db_data = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES where deleted is NULL and database_name not in ('SNOWFLAKE','SNOWFLAKE_SAMPLE_DATA');",conn)
+dbs = list(set(list(db_data['DATABASE'])))
+DB = st.sidebar.selectbox('select database:',dbs)
+st.sidebar.write('Selected Database :', DB)
 
-sc= pd.read_sql("select CATALOG_NAME AS DATABASE,SCHEMA_NAME AS SCHEMA from {}.information_schema.SCHEMATA where SCHEMA_NAME NOT IN ('INFORMATION_SCHEMA');".format(option),conn)
+sc= pd.read_sql("select CATALOG_NAME AS DATABASE,SCHEMA_NAME AS SCHEMA from {}.information_schema.SCHEMATA where SCHEMA_NAME NOT IN ('INFORMATION_SCHEMA');".format(DB),conn)
 sc
 scs = list(set(list(sc['SCHEMA'])))
 next = st.selectbox('select schema:',scs)
 st.write('Selected Schema:', next)
 
-tab = pd.read_sql("select TABLE_CATALOG AS DATABASE,TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES;".format(option),conn) 
+tab = pd.read_sql("select TABLE_CATALOG AS DATABASE,TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES;".format(DB),conn) 
 tab = tab.loc[tab['SCHEMA']==next]
 tab
 tabs = list(set(list(tab['TABLE_NAME'])))
 final = st.selectbox('select table:',tabs)
 st.write('Selected Table:', final)
 
-conn.cursor().execute("call ASSOCIATE_SEMANTIC_CATEGORY_TAGS('{}.{}.{}',EXTRACT_SEMANTIC_CATEGORIES('{}.{}.{}'))".format(option,next,final,option,next,final));
+conn.cursor().execute("call ASSOCIATE_SEMANTIC_CATEGORY_TAGS('{}.{}.{}',EXTRACT_SEMANTIC_CATEGORIES('{}.{}.{}'))".format(DB,next,final,DB,next,final));
 
 
 
-val = pd.read_sql("select COLUMN_NAME,TAG_NAME,TAG_VALUE from table({}.information_schema.tag_references_all_columns('{}.{}.{}', 'table'));".format(option,option,next,final),conn)
+val = pd.read_sql("select COLUMN_NAME,TAG_NAME,TAG_VALUE from table({}.information_schema.tag_references_all_columns('{}.{}.{}', 'table'));".format(DB,DB,next,final),conn)
 val = val.pivot(index=['COLUMN_NAME'],columns=['TAG_NAME'],values=['TAG_VALUE']).reset_index()
 val
 
