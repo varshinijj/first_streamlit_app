d5708ceebbaf3dfe46489adb7ceaed49d39e6e71
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 16:51:45 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -7,7 +7,6 @@ conn = snowflake.connector.connect(
                 user='VARSHINI',
                 password='Snowflake@22!',
                 account='bg35464.ap-southeast-1',
-                warehouse = 'UI',
                 ocsp_fail_open=False)
 
 db_data = pd.read_sql("select database_name as database from SNOWFLAKE.ACCOUNT_USAGE.DATABASES;",conn)
