01cea539904838f9e8bc767329daf60a0ff4e79f
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 17:09:08 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -56,6 +56,7 @@ with d.subgraph() as s:
     s.attr(rank='same')
     for idx,row in tags_semantic.iterrows():
         s.node('{}'.format(row['TAG_VALUE']))
+        d.edge('{}'.format(row['COLUMN_NAME']),'{}'.format(row['TAG_VALUE']))
        
 st.graphviz_chart(d)
 
