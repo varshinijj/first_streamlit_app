2e13dc865d768d5b3d8c99b84625c543032bbbc7
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 22:49:21 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import plotly.graph_objects as go
 import graphviz as graphviz
 import pandas as pd
+st.set_page_config(layout="wide")
 import snowflake.connector
 conn = snowflake.connector.connect(
                 user='VARSHINI',
