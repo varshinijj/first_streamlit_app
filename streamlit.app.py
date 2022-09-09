dceaefca45816c0d4f0e96fb1e5de2873f87f7ef
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-09 14:44:26 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -52,6 +52,7 @@ with d.subgraph() as s:
         s.node('{}'.format(str(row['COLUMN_NAME']).split()[1]))
         d.edge('{}'.format(str(row['TABLE_NAME']).split()[1]),'{}'.format(str(row['COLUMN_NAME']).split()[1]))
 with d.subgraph() as s:
+    s.attr(rank='same')
     for idx,row in tags_tb.iterrows():
         s.node('{}'.format(str(row['TAG_VALUE']).split()[1]))
         d.edge('{}'.format(str(row['TAG_VALUE']).split()[1]),'{}'.format(str(row['COLUMN_NAME']).split()[1]))        
