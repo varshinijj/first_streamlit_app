1a330e668d7c7bac60928e80d0ac794161976a2c
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-11 14:38:31 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -44,15 +44,17 @@ with col1:
   click = st.radio('Choose Schemas:',select)
   if click =='All Schemas':
     sc =  pd.read_sql("select CATALOG_NAME AS DATABASE,SCHEMA_NAME AS SCHEMA from {}.information_schema.SCHEMATA where SCHEMA_NAME !='INFORMATION_SCHEMA';".format(DB),conn)
-    sc_tb = pd.read_sql("select TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES where TABLE_SCHEMA != 'INFORMATION_SCHEMA';".format(DB),conn) 
-    tags_tb_grouped = tags_tb1.groupby(['SCHEMA','TABLE_NAME']).size().reset_index(name='no.of.sensitive_col')
+    sc_tb = pd.read_sql("select TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES where TABLE_SCHEMA != 'INFORMATION_SCHEMA';".format(DB),conn)
+    if classify:
+      tags_tb_grouped = tags_tb1.groupby(['SCHEMA','TABLE_NAME']).size().reset_index(name='no.of.sensitive_col')
   else:
     for x in list(sc['SCHEMA']):
       schemas = st.checkbox('{}'.format(x),False)
       if schemas==False:
         sc = sc.loc[sc['SCHEMA']!=x]
         sc_tb = sc_tb.loc[sc_tb['SCHEMA']!=x]
-        tags_tb_grouped = tags_tb_grouped.loc[tags_tb_grouped['SCHEMA']!=x] 
+        if classify:
+          tags_tb_grouped = tags_tb_grouped.loc[tags_tb_grouped['SCHEMA']!=x] 
        
 
   d = graphviz.Digraph()
