e0cb09024d7454b2dbde527990e968b46751b85e
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-11 16:06:53 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -47,6 +47,7 @@ with col1:
   classify = st.button('Classify')
   if classify:
     for idx,row in sc_tb.iterrows():
+      print(row['SCHEMA'],row['TABLE_NAME']
       conn.cursor().execute("call ASSOCIATE_SEMANTIC_CATEGORY_TAGS('{}.{}.{}',EXTRACT_SEMANTIC_CATEGORIES('{}.{}.{}'))".format(DB,row['SCHEMA'],row['TABLE_NAME'],DB,row['SCHEMA'],row['TABLE_NAME']));
     tags = pd.read_sql("select OBJECT_SCHEMA as schema,OBJECT_NAME as table_name,COLUMN_NAME,TAG_NAME,TAG_VALUE from table({}.information_schema.tag_references_all_columns('{}.{}.{}','table'));".format(DB,DB,row['SCHEMA'],row['TABLE_NAME']),conn) 
     tags_pivot = tags.pivot(index=['SCHEMA','TABLE_NAME','COLUMN_NAME'],columns=['TAG_NAME'],values=['TAG_VALUE']).reset_index()
