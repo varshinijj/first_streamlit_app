37612285f9735033fe5cabc1079d68a2b4a9c4a1
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-12 18:41:00 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -38,9 +38,17 @@ else:
   conn.cursor().execute("alter warehouse SQLWH set warehouse_size = SMALL MAX_CLUSTER_COUNT =2 MIN_CLUSTER_COUNT =1;")
 
 ####schemas and tables in the database are queried####   
+@st.experimental_memo
+def sc():
+  sc = pd.read_sql("select CATALOG_NAME AS DATABASE,SCHEMA_NAME AS SCHEMA from {}.information_schema.SCHEMATA where SCHEMA_NAME !='INFORMATION_SCHEMA';".format(DB),conn)
+  return sc
+sc = sc()
+@st.experimental_memo
+def sc_tb():
+  sc_tb = pd.read_sql("select TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES where TABLE_SCHEMA != 'INFORMATION_SCHEMA';".format(DB),conn)
+  return sc_tb
+sc_tb = sc_tb()
 
-sc = pd.read_sql("select CATALOG_NAME AS DATABASE,SCHEMA_NAME AS SCHEMA from {}.information_schema.SCHEMATA where SCHEMA_NAME !='INFORMATION_SCHEMA';".format(DB),conn)
-sc_tb = pd.read_sql("select TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES where TABLE_SCHEMA != 'INFORMATION_SCHEMA';".format(DB),conn)
 
 ####separating layout into 3 columns####
 
@@ -55,8 +63,8 @@ with col1:
   select = ['All Schemas','Select Schemas']
   click = st.radio('Choose Schema:',select)
   if click =='All Schemas':
-    sc =  pd.read_sql("select CATALOG_NAME AS DATABASE,SCHEMA_NAME AS SCHEMA from {}.information_schema.SCHEMATA where SCHEMA_NAME !='INFORMATION_SCHEMA';".format(DB),conn)
-    sc_tb = pd.read_sql("select TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES where TABLE_SCHEMA != 'INFORMATION_SCHEMA';".format(DB),conn)
+    sc = sc() 
+    sc_tb = sc_tb()
   else:
     for x in list(sc['SCHEMA']):
       schemas = st.checkbox('{}'.format(x),False)
