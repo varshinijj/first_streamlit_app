341fd235c348e61cc449f353598f95d13db687f5
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-19 21:10:27 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -67,20 +67,20 @@ with col1:
       
 ####Classifying tables in schemas selected and applying tags on columns####
 
-  classify = st.button('Classify')
-  if classify:
-    if sc.shape[0] ==0:
+  if sc.shape[0] ==0:
       st.error('A Schema has to be selected', icon="🚨")
-    else:
-      alltags = pd.DataFrame(columns=['SCHEMA', 'TABLE_NAME', 'COLUMN_NAME','TAG_NAME','TAG_VALUE'])
-      for idx,row in sc_tb.iterrows():
-        conn.cursor().execute("call ASSOCIATE_SEMANTIC_CATEGORY_TAGS('{}.{}.{}',EXTRACT_SEMANTIC_CATEGORIES('{}.{}.{}'));".format(DB,row['SCHEMA'],row['TABLE_NAME'],DB,row['SCHEMA'],row['TABLE_NAME']))
-        tags = pd.read_sql("select OBJECT_SCHEMA as schema,OBJECT_NAME as table_name,COLUMN_NAME,TAG_NAME,TAG_VALUE from table({}.information_schema.tag_references_all_columns('{}.{}.{}','table'));".format(DB,DB,row['SCHEMA'],row['TABLE_NAME']),conn) 
-        alltags = alltags.append(tags, ignore_index=True) 
-      st.success('Tags applied!', icon="✅")  
-      tags_pivot = alltags.pivot(index=['SCHEMA','TABLE_NAME','COLUMN_NAME'],columns=['TAG_NAME'],values=['TAG_VALUE']).reset_index()
-      tags_tb = tags_pivot[['SCHEMA','TABLE_NAME']]
-      tags_tb_grouped = tags_tb.groupby(['SCHEMA','TABLE_NAME']).size().reset_index(name='no.of.sensitive_col')
+  else:
+    classify = st.button('Classify')
+  
+  if classify:
+    alltags = pd.DataFrame(columns=['SCHEMA', 'TABLE_NAME', 'COLUMN_NAME','TAG_NAME','TAG_VALUE'])
+    for idx,row in sc_tb.iterrows():
+      conn.cursor().execute("call ASSOCIATE_SEMANTIC_CATEGORY_TAGS('{}.{}.{}',EXTRACT_SEMANTIC_CATEGORIES('{}.{}.{}'));".format(DB,row['SCHEMA'],row['TABLE_NAME'],DB,row['SCHEMA'],row['TABLE_NAME']))        tags = pd.read_sql("select OBJECT_SCHEMA as schema,OBJECT_NAME as table_name,COLUMN_NAME,TAG_NAME,TAG_VALUE from table({}.information_schema.tag_references_all_columns('{}.{}.{}','table'));".format(DB,DB,row['SCHEMA'],row['TABLE_NAME']),conn) 
+      alltags = alltags.append(tags, ignore_index=True) 
+    st.success('Tags applied!', icon="✅")  
+    tags_pivot = alltags.pivot(index=['SCHEMA','TABLE_NAME','COLUMN_NAME'],columns=['TAG_NAME'],values=['TAG_VALUE']).reset_index()
+    tags_tb = tags_pivot[['SCHEMA','TABLE_NAME']]
+    tags_tb_grouped = tags_tb.groupby(['SCHEMA','TABLE_NAME']).size().reset_index(name='no.of.sensitive_col')
       
 ####Removing the applied tags from tables####
 
@@ -152,6 +152,9 @@ with col2:
 
 with col3:
   st.write("masking policy options")
+  
+  
+  
   
 
 
