73bd780279daaf8df595dad6a8cd40f38666fac0
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-22 00:09:02 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -151,7 +151,7 @@ with tab1:
         for i, row in roles_acc.iterrows():
           R.append(row['NAME'])
         roles = st.multiselect('Choose Roles that can see the data:',R)
-        sroles = (str(roles)[2:-2]).replace(' ',',')
+        sroles = (str(roles)[2:-2]).replace(' ',',','"',''')
         mdatatype = st.radio('Choose Datatype:',['String','Number'])
         if (mdatatype=='String' and str(final4dt).split()[1]=='TEXT') or (mdatatype =='Number' and str(final4dt).split()[1]=='NUMBER'):
           if st.button('Create and Apply Mask'):
