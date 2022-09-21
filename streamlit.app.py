1f3248a27349b28ffe939efca660fa59fd71c96e
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-21 11:53:10 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -158,9 +158,9 @@ with tab1:
     else:
       if classify==True:
         display=pd.merge(sc,tags_pivot, on=['SCHEMA'], how='inner').rename(columns={('TABLE_NAME',''):'TABLE NAME',('COLUMN_NAME',''):'COLUMN NAME',('TAG_VALUE','SEMANTIC_CATEGORY'):'SEMANTIC CATEGORY',('TAG_VALUE','PRIVACY_CATEGORY'):'PRIVACY CATEGORY'})
-        display
-        alldatatypes
-              
+        
+        final = pd.merge(display,alldatatypes,left_on=['DATABASE','SCHEMA','TABLE NAME','COLUMN NAME'],right_on=['DATABASE','SCHEMA','TABLE NAME','COLUMN_NAME'] how = 'left')
+        final      
 ####col3---masking policy options####  
 with tab1:
   with col2:
