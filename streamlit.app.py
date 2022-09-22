e32731c7a43625ef5a6767acb963eef7ab9f365c
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-22 21:29:04 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -31,12 +31,7 @@ def schema_sc():
   sc = pd.read_sql("select CATALOG_NAME AS DATABASE,SCHEMA_NAME AS SCHEMA from {}.information_schema.SCHEMATA where SCHEMA_NAME !='INFORMATION_SCHEMA';".format(DB),conn)
   return sc
 
-def schema_sc_tb():
-  sc = schema_sc()
-  sc_tb = pd.read_sql("select TABLE_SCHEMA AS SCHEMA,TABLE_NAME from {}.information_schema.TABLES where TABLE_SCHEMA != 'INFORMATION_SCHEMA';".format(DB),conn)
-  return sc_tb
-
-sc_tb = schema_sc_tb()
+sc = schema_sc()
 
 
 
