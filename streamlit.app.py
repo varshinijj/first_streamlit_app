54d816e4383f2dd53c544d9ddb816c6e501db1b4
varshinijj <113025704+varshinijj@users.noreply.github.com>
2022-09-22 10:22:09 +0530

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -177,6 +177,7 @@ with tab1:
           for i,row in sc.iterrows():
             policy_tab = pd.read_sql("select Policy_db as database,policy_schema as schema , ref_schema_name as table_name, ref_column_name as column_name,policy_name  from SNOWFLAKE.ACCOUNT_USAGE.POLICY_REFERENCES where Policy_db ='{}' and policy_schema='{}';".format(DB,row['SCHEMA']),conn)
             allpolicy_tab.append(policy_tab)
+            policy_tab
           allpolicy_tab  
           policy = st.selectbox('Çhoose Masking Policy:',list(set(allpolicy_tab['POLICY_NAME'])))
           ed = st.radio('',['Edit Mask','Drop Mask'])
